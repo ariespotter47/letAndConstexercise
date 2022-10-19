c2b4e9bc5254d54dfc6a054e4d6158a7650d72c0
ariespotter47 <91033731+ariespotter47@users.noreply.github.com>
2022-10-18 23:40:21 -0400

    Add files via upload

---
+++ letAndConst.js
@@ -0,0 +1,9 @@
+ const PI = 3.14;
+ PI = 42;
+
+
+ //quiz answer
+ // var can be updated and redeclared in it's scope.
+ //let can be updated but not redeclared.
+ //const can not be updated or redeclared.
+ //hoisting is where we can move the delcaration of function, variables or classed to the top of the scope, before the code can run.
